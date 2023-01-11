3430847db21008de9d5d9ec43106b9335a600d0a
FierSet <66457209+FierSet@users.noreply.github.com>
2023-01-11 11:53:04 -0600

    Add files via upload

---
+++ Cesar/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+using System;
+
+namespace Cesar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buscar = new System.Windows.Forms.Button();
+            this.encriptar = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.key_list = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.path_box = new System.Windows.Forms.TextBox();
+            this.textarea = new System.Windows.Forms.RichTextBox();
+            this.desencriptar = new System.Windows.Forms.Button();
+            this.guarda = new System.Windows.Forms.Button();
+            this.bruta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buscar
+            // 
+            this.buscar.Location = new System.Drawing.Point(203, 18);
+            this.buscar.Name = "buscar";
+            this.buscar.Size = new System.Drawing.Size(75, 23);
+            this.buscar.TabIndex = 1;
+            this.buscar.Text = "Buscar";
+            this.buscar.UseVisualStyleBackColor = true;
+            this.buscar.Click += new System.EventHandler(this.buscar_Click);
+            // 
+            // encriptar
+            // 
+            this.encriptar.Location = new System.Drawing.Point(166, 49);
+            this.encriptar.Name = "encriptar";
+            this.encriptar.Size = new System.Drawing.Size(75, 23);
+            this.encriptar.TabIndex = 2;
+            this.encriptar.Text = "Encriptar";
+            this.encriptar.UseVisualStyleBackColor = true;
+            this.encriptar.Click += new System.EventHandler(this.encriptar_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(60, 78);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // key_list
+            // 
+            this.key_list.FormattingEnabled = true;
+            this.key_list.Location = new System.Drawing.Point(60, 51);
+            this.key_list.Name = "key_list";
+            this.key_list.Size = new System.Drawing.Size(65, 21);
+            this.key_list.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "KEY:";
+            // 
+            // path_box
+            // 
+            this.path_box.Location = new System.Drawing.Point(12, 20);
+            this.path_box.Name = "path_box";
+            this.path_box.Size = new System.Drawing.Size(185, 20);
+            this.path_box.TabIndex = 0;
+            // 
+            // textarea
+            // 
+            this.textarea.Location = new System.Drawing.Point(12, 107);
+            this.textarea.Name = "textarea";
+            this.textarea.Size = new System.Drawing.Size(346, 198);
+            this.textarea.TabIndex = 6;
+            this.textarea.Text = "";
+            // 
+            // desencriptar
+            // 
+            this.desencriptar.Location = new System.Drawing.Point(247, 49);
+            this.desencriptar.Name = "desencriptar";
+            this.desencriptar.Size = new System.Drawing.Size(75, 23);
+            this.desencriptar.TabIndex = 7;
+            this.desencriptar.Text = "desencriptar";
+            this.desencriptar.UseVisualStyleBackColor = true;
+            this.desencriptar.Click += new System.EventHandler(this.desencriptar_Click);
+            // 
+            // guarda
+            // 
+            this.guarda.Location = new System.Drawing.Point(247, 78);
+            this.guarda.Name = "guarda";
+            this.guarda.Size = new System.Drawing.Size(75, 23);
+            this.guarda.TabIndex = 8;
+            this.guarda.Text = "Guardar";
+            this.guarda.UseVisualStyleBackColor = true;
+            this.guarda.Click += new System.EventHandler(this.guarda_Click);
+            // 
+            // bruta
+            // 
+            this.bruta.Location = new System.Drawing.Point(166, 78);
+            this.bruta.Name = "bruta";
+            this.bruta.Size = new System.Drawing.Size(75, 23);
+            this.bruta.TabIndex = 9;
+            this.bruta.Text = "FB.";
+            this.bruta.UseVisualStyleBackColor = true;
+            this.bruta.Click += new System.EventHandler(this.bruta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 317);
+            this.Controls.Add(this.bruta);
+            this.Controls.Add(this.guarda);
+            this.Controls.Add(this.desencriptar);
+            this.Controls.Add(this.textarea);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.key_list);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.encriptar);
+            this.Controls.Add(this.buscar);
+            this.Controls.Add(this.path_box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buscar;
+        private System.Windows.Forms.Button encriptar;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox key_list;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox path_box;
+        private System.Windows.Forms.RichTextBox textarea;
+        private System.Windows.Forms.Button desencriptar;
+        private System.Windows.Forms.Button guarda;
+        private System.Windows.Forms.Button bruta;
+    }
+}
+
---
+++ Cesar/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cesar
+{
+    public partial class Form1 : Form
+    {
+        private String path;
+        private String subrutaguarda, NombreArchivo, Extencionesg;
+        private int key;
+        byte[] Textobyte = null;
+        String textos_g = "";
+        public String[] abc = new string[] 
+        {
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","Ñ",
+            "`","|","^","_","~","@","-","´","+","*","¨","[","]","{","}","¡","!","¿","?","=","(",")","&","%","$","#","\'","\"","°","\\","/",":",".",";",",","\n", " ",
+            "0","1","2","3","4","5","6","7","8","9",
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            Inicio();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cobobox();
+        }
+
+        public void Inicio()
+        {
+            //path_box.Enabled = false;
+
+        }
+
+        public void BuscaArchivo()
+        {
+            textarea.ForeColor = Color.Black;
+            OpenFileDialog busca = new OpenFileDialog();
+            busca.Filter = "All files (*.*)|*.*" + "|" + "TXT(*.txt)|*.txt"; // "PDF(*.pdf)|*.pdf|" + |DOCX(*.docx)|*.docx
+            busca.FilterIndex = 2;
+
+            if (busca.ShowDialog() == DialogResult.OK)
+            {
+                path_box.Text = busca.FileName;
+                path = busca.FileName;
+            }
+            textos_g = Leer();
+            textarea.Text = textos_g;
+        }
+
+        public void buscarutaG()
+        {
+            FolderBrowserDialog buscar = new FolderBrowserDialog();
+
+            if (buscar.ShowDialog() == DialogResult.OK)
+            {
+                subrutaguarda = buscar.SelectedPath;
+                guardar();
+            }
+        }
+
+        public void guardar()
+        {
+            if (comprobararchivo())
+            {
+                textarea.ForeColor = Color.Green;
+                Textobyte = Encoding.ASCII.GetBytes(textos_g);
+                Escribe();
+                Mensajebox("Datos guardado en: " + subrutaguarda + "\\" + NombreArchivo + "." + Extencionesg, "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                Mensajebox("No se puede Guardar", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public void Escribe()
+        {                                                                  //Extencionesg
+            File.WriteAllBytes(subrutaguarda + "\\" + NombreArchivo + "." + "txt", Textobyte);
+        }
+
+        public string Leer()//abre el archivo
+        {
+            Textobyte = File.ReadAllBytes(path);
+            return System.Text.Encoding.UTF8.GetString(Textobyte);
+        }
+
+        public bool comprobararchivo() // verifica la compatibilidad
+        {
+            bool cumple = false;
+            try
+            {
+                NombreArchivo = path.Substring(path.LastIndexOf(((char)92)) + 1);
+                int index = NombreArchivo.LastIndexOf('.');
+                Extencionesg = NombreArchivo.Substring(index + 1);
+                NombreArchivo = NombreArchivo.Substring(0, index);
+
+                cumple = true;
+
+            }
+            catch (Exception e)
+            {
+                textarea.ForeColor = Color.Red;
+                textarea.Text = "\n\t=-=-=-=-=-=-=\'" + path + "\' No es una ruta Correcta=-=-=-=-=-=-=\n" + e.ToString();
+            }
+            return cumple;
+        }
+
+        public void Cobobox()
+        {
+            for (int i = 1; i < abc.Length; i++)
+                key_list.Items.Add(i);
+        }
+
+        public void Cesar()
+        {
+            String texto = textos_g;
+            string textos = "";
+
+            Invoke(new Action(() =>
+            {
+                progressBar1.Value = 0;
+                progressBar1.Maximum = texto.Length;
+            }));
+
+            foreach (char charac in texto)
+            {
+                for(int i = 0; i < abc.Length; i++)
+                    if(abc[i] == charac.ToString())
+                    {
+                        if((i + key) >= abc.Length)
+                            textos += abc[key -  (abc.Length - i)];
+                        else
+                            textos += abc[key + i];
+                        break;
+                    }
+                Invoke(new Action(() => progressBar1.Value++));
+            }
+            textos_g = textos;
+            Invoke(new Action(() =>
+            {
+                textarea.Text = textos_g;
+                bloqueo(true);
+            }));
+        }
+
+        public void Descesar()
+        {
+            String texto = textos_g;
+            string textos = "";
+            
+            Invoke(new Action(() =>
+            {
+                progressBar1.Value = 0;
+                progressBar1.Maximum = texto.Length;
+            }));
+
+            foreach (char charac in texto)
+            {
+                for (int i = 0; i < abc.Length; i++)
+                    if (abc[i] == charac.ToString())
+                    {
+                        if ((i - key) < 0)
+                            textos += abc[abc.Length + (i - key)];
+                        else
+                            textos += abc[i - key];
+                        break;
+                    }
+                Invoke(new Action(() => progressBar1.Value++));
+            }
+            textos_g = textos;
+            Invoke(new Action(() =>
+            {
+                textarea.Text = textos_g;
+                bloqueo(true);
+            }));
+        }
+
+        public void fuerzabruta()
+        {
+            String texto = textos_g;
+            string textos = "";
+
+            Invoke(new Action(() =>
+            {
+                progressBar1.Value = 0;
+                progressBar1.Maximum = abc.Length;
+            }));
+
+            for(int k = 0; k < abc.Length; k++)
+            {
+                textos += "key: " + k + "\n____________________________________________________\n";
+                foreach (char charac in texto)
+                {
+                    for (int i = 0; i < abc.Length; i++)
+                        if (abc[i] == charac.ToString())
+                        {
+                            if ((i - k) < 0)
+                                textos += abc[abc.Length + (i - k)];
+                            else
+                                textos += abc[i - k];
+                            break;
+                        }
+                }
+                textos += "\n______________________________________________________\n";
+                Invoke(new Action(() => progressBar1.Value++));
+            }
+
+            textos_g = textos;
+            Invoke(new Action(() =>
+            {
+                textarea.Text = textos_g;
+                bloqueo(true);
+            }));
+        }
+
+        private void buscar_Click(object sender, EventArgs e)
+        {
+            BuscaArchivo();
+        }
+
+        private void encriptar_Click(object sender, EventArgs e)
+        {
+            if (textos_g != string.Empty)
+                if ((key_list.SelectedItem != null))
+                {
+                    key = (int)key_list.SelectedItem;
+                    bloqueo(false);
+                    Thread cesarenc = new Thread(Cesar);
+                    cesarenc.Start();
+                }
+                else
+                    Mensajebox("No selecciono key: ", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+                Mensajebox("No selecciono Ruta: ", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void desencriptar_Click(object sender, EventArgs e)
+        {
+            if (textos_g != string.Empty)
+                if ((key_list.SelectedItem != null))
+                {
+                    key = (int)key_list.SelectedItem;
+                    bloqueo(false);
+                    Thread descesarenc = new Thread(Descesar);
+                    descesarenc.Start();
+                }
+                else
+                    Mensajebox("No selecciono key: ", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+                Mensajebox("No selecciono Ruta: ", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void guarda_Click(object sender, EventArgs e)
+        {
+            buscarutaG();
+        }
+
+        private void bruta_Click(object sender, EventArgs e)
+        {
+            if (textos_g != string.Empty)
+            {
+                bloqueo(false);
+                Thread FB = new Thread(fuerzabruta);
+                FB.Start();
+            }
+            else
+                Mensajebox("No selecciono Ruta: ", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public void bloqueo(bool estado)
+        {
+            buscar.Enabled = estado;
+            key_list.Enabled = estado;
+            encriptar.Enabled = estado;
+            desencriptar.Enabled = estado;
+        }
+
+        public DialogResult Mensajebox(string mensaje, string titulo, MessageBoxButtons botones, MessageBoxIcon icono)
+        {
+            return MessageBox.Show(mensaje, titulo, botones, icono);
+        }
+    }
+}
